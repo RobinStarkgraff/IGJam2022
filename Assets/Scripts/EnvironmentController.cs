62a5d0cad6a1d29deb13ea323db5166b12550f6e
robin.starkgraff <rstarkgraff@gmx.net>
2022-11-12 20:02:18 +0100

    Prepare ground changes

---
+++ Assets/Scripts/EnvironmentController.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+    public static EnvironmentController Instance;
+    
+    private Material mat;
+    private float width;
+    private float progress;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        width = mat.mainTextureScale.x;
+    }
+
+    public void UpdateTexture(float relativeSpeed)
+    {
+        progress += relativeSpeed;
+        float x = progress % width;
+        mat.mainTextureOffset = new Vector2(x, 0);
+    }
+}
